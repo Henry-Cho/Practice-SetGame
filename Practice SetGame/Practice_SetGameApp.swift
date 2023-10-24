a5da2acd6b8e8ac83fe3ace966e5983bca5b9956
Henry Cho <hyungseok5926@gmail.com>
2023-10-24 05:30:30 -0600

    Initial Commit

---
+++ Practice SetGame/Practice_SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_SetGameApp.swift
+//  Practice SetGame
+//
+//  Created by Henry Cho on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
